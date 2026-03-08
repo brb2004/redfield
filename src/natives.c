ad82712928425c1d18c465e6c23c0040ee83103a
Brb2004 <braydenbenard7@gmail.com>
2026-03-07 19:04:37 -0600

    feat: lazy OpenGL init, window only opens when script uses graphics

--- src/natives.c
+++ src/natives.c
@@ -11,23 +11,29 @@
 #include <math.h>
 
 extern GLFWwindow* window;
+extern void initOpenGL();
+#define ENSURE_WINDOW() do { if (window == NULL) initOpenGL(); } while(0)
 
 static Value windowShouldCloseNative(int argCount, Value* args) {
     (void)argCount; (void)args;
+    ENSURE_WINDOW();
     return BOOL_VAL(glfwWindowShouldClose(window));
 }
 static Value pollEventsNative(int argCount, Value* args) {
     (void)argCount; (void)args;
+    ENSURE_WINDOW();
     glfwPollEvents();
     return NIL_VAL;
 }
 static Value swapBuffersNative(int argCount, Value* args) {
     (void)argCount; (void)args;
+    ENSURE_WINDOW();
     glfwSwapBuffers(window);
     return NIL_VAL;
 }
 static Value clearColorNative(int argCount, Value* args) {
     (void)argCount;
+    ENSURE_WINDOW();
     float r = (float)AS_NUMBER(args[0]);
     float g = (float)AS_NUMBER(args[1]);
     float b = (float)AS_NUMBER(args[2]);
